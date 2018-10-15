269b4d601c58792bc32687c4de697885f4c0fcf9
张超逸 <1459580961@qq.com>
2018-10-15 20:48:24 +0800

    Update LongestSubstringWithoutRepeatingCharacters.py

--- Longest Substring Without Repeating Characters/LongestSubstringWithoutRepeatingCharacters.py
+++ Longest Substring Without Repeating Characters/LongestSubstringWithoutRepeatingCharacters.py
@@ -7,8 +7,8 @@ class Solution:
         :rtype: int
         """
 
-        temp_list = []
-        max_length, length = 0, 0
+        temp_list = []  # 存储子字符串
+        max_length, length = 0, 0  # length跟踪子字符串的长度
 
         for letter in s:
             if letter not in temp_list:
@@ -16,6 +16,8 @@ class Solution:
                 if length > max_length:
                     max_length = length
             else:
+                # 这里由于后边的字符的最大不重复子字符串不可能超过第一个
+                # 所以要从第一个已经的不重复的字符开始计算
                 temp_list = temp_list[temp_list.index(letter) + 1:]
                 length = len(temp_list) + 1
             temp_list.append(letter)
