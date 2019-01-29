efe1049f2cac6ac37f274ec086d130ad657326f9
张超逸 <1459580961@qq.com>
2019-01-29 23:10:46 +0800

    Update Candy.py

--- Candy/Candy.py
+++ Candy/Candy.py
@@ -1,3 +1,4 @@
+# 思路一的py代码
 # 80ms 44.90%
 
 class Solution:
@@ -17,4 +18,30 @@ class Solution:
                 numCandy[i] = numCandy[i + 1] + 1
                 
         return sum(numCandy)
-        
+
+# 思路二的py代码
+# 56ms 100%
+
+class Solution:
+    def candy(self, ratings):
+        sum, pre, count = 1, 1, 1
+        for i in range(1, len(ratings)):
+            if ratings[i] >= ratings[i - 1]:
+                if count > 1:
+                    sum += count * (count - 1) // 2
+                    if (pre < count):
+                        sum += (count - pre)
+                    pre = 1
+                if (ratings[i] == ratings[i - 1]):
+                    pre = 0
+
+                pre += 1
+                sum += pre
+                count = 1
+            else:
+                count += 1
+        if count > 1:
+            sum += count * (count - 1) // 2
+            if (pre < count):
+                sum += (count - pre)
+        return sum
