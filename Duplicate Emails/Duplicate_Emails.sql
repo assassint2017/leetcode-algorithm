dd03825b840cbf35d5b4b8452d718533c1f86447
张超逸 <1459580961@qq.com>
2019-02-26 19:57:13 +0800

    Update Duplicate_Emails.sql

--- Duplicate Emails/Duplicate_Emails.sql
+++ Duplicate Emails/Duplicate_Emails.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
 # Runtime: 206 ms, faster than 29.08% of MySQL online submissions for Duplicate Emails.
-select Email from Person group by Email having count(distinct Id) > 1;
+select Email from Person group by Email having count(*) > 1;
