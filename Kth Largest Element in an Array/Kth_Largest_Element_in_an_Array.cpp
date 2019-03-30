9a1dbed9115069e041a383ec70567c10d46834c3
张超逸 <1459580961@qq.com>
2019-03-30 11:45:16 +0800

    Update Kth_Largest_Element_in_an_Array.cpp

--- Kth Largest Element in an Array/Kth_Largest_Element_in_an_Array.cpp
+++ Kth Largest Element in an Array/Kth_Largest_Element_in_an_Array.cpp
@@ -70,3 +70,67 @@ public:
         maxHeap[parent] = temp;
     }
 };
+
+// 第二种思路
+// 使用快速排序的思想二分数据，达到线性的时间复杂度
+// Runtime: 12 ms, faster than 98.21% of C++ online submissions for Kth Largest Element in an Array.
+// Memory Usage: 9.5 MB, less than 56.68% of C++ online submissions for Kth Largest Element in an Array.
+class Solution 
+{
+public:
+    int findKthLargest(vector<int>& nums, int k) 
+    {
+        return sort(nums, k, 0, nums.size() - 1);
+    }
+private:
+    int sort(vector<int>& nums, int k, int left, int right)
+    {
+        if (left == right)
+            return nums[left];
+        
+        int pivot = median3(nums, left, right);
+        if (right - left == 1)
+        {
+            if (left == nums.size() - k)
+                return nums[left];
+            else
+                return nums[right];
+        }
+        
+        int leftptr = left, rightptr = right - 1;
+        
+        while (true)
+        {
+            while (nums[++leftptr] < pivot);
+            while (rightptr > leftptr && nums[--rightptr] > pivot);
+            
+            if (leftptr < rightptr)
+                swap(nums[leftptr], nums[rightptr]);
+            else
+                break;
+        }
+        if (leftptr < right)
+            swap(nums[right - 1], nums[leftptr]);
+        
+        if (leftptr > nums.size() - k)
+            return sort(nums, k, left, leftptr - 1);
+        else if (leftptr < nums.size() - k)
+            return sort(nums, k, leftptr + 1, right);
+        else
+            return pivot;
+    }
+    
+    int median3(vector<int>& nums, int left, int right)
+    {
+        int mid = (left + right) / 2;
+        if (nums[left] > nums[mid])
+            swap(nums[left], nums[mid]);
+        if (nums[left] > nums[right])
+            swap(nums[left], nums[right]);
+        if (nums[mid] > nums[right])
+            swap(nums[mid], nums[right]);
+        
+        swap(nums[mid], nums[right - 1]);
+        return nums[right - 1];
+    }
+};
