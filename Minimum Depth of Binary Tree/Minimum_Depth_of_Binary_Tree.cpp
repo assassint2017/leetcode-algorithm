391c30253213c420642ddab54988cec00521239f
张超逸 <1459580961@qq.com>
2019-03-31 16:53:27 +0800

    Update Minimum_Depth_of_Binary_Tree.cpp

--- Minimum Depth of Binary Tree/Minimum_Depth_of_Binary_Tree.cpp
+++ Minimum Depth of Binary Tree/Minimum_Depth_of_Binary_Tree.cpp
@@ -30,3 +30,49 @@ public:
             return depthLeft < depthRight ? depthLeft + 1: depthRight + 1;
     }
 };
+
+// 典型的使用bfs来进行查找\
+// Runtime: 16 ms, faster than 99.26% of C++ online submissions for Minimum Depth of Binary Tree.
+// Memory Usage: 19.4 MB, less than 96.36% of C++ online submissions for Minimum Depth of Binary Tree.
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution 
+{
+public:
+    int minDepth(TreeNode* root) 
+    {
+        if (!root)
+            return 0;
+        
+        queue<TreeNode*> treeQueue;
+        treeQueue.push(root);
+        
+        int layer = 1;
+        while (!treeQueue.empty())
+        {
+            for (int i = treeQueue.size(); i > 0; i--)
+            {
+                root = treeQueue.front();
+                treeQueue.pop();
+
+                if(root->left == nullptr && root->right == nullptr)
+                    return layer;
+                
+                if (root->left) treeQueue.push(root->left);
+                if (root->right) treeQueue.push(root->right);
+            }
+            layer++;
+        }
+        
+        // 这里如果不加的话，编译可能会出现问题，因为可能会出现没有返回值的情况
+        return layer;
+    }
+};
