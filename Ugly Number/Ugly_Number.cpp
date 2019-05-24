688e41c98b415e459d6ba9ef5a0ff90c0d69e483
张超逸 <1459580961@qq.com>
2019-05-24 15:20:31 +0800

    Update Ugly_Number.cpp

--- Ugly Number/Ugly_Number.cpp
+++ Ugly Number/Ugly_Number.cpp
@@ -29,3 +29,27 @@ public:
             return false;       
     }
 };
+
+// 下面是更加高效的版本的代码
+// Runtime: 4 ms, faster than 92.43% of C++ online submissions for Ugly Number.
+// Memory Usage: 8.2 MB, less than 5.11% of C++ online submissions for Ugly Number.
+class Solution 
+{
+public:
+    bool isUgly(int num) 
+    {
+        // 边界条件处理
+        if (num <= 0)
+            return false;
+
+        // 开始进行循环计算
+        while (num % 2 == 0)     
+            num /= 2;
+        while (num % 3 == 0)
+            num /= 3;
+        while (num % 5 == 0)
+            num /= 5;
+        
+        return num == 1;
+    }
+};
