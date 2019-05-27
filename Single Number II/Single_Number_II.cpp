c5b10e526c24959c53aff7015d010a505401d5d2
张超逸 <1459580961@qq.com>
2019-05-27 13:16:14 +0800

    Update Single_Number_II.cpp

--- Single Number II/Single_Number_II.cpp
+++ Single Number II/Single_Number_II.cpp
@@ -12,21 +12,21 @@ public:
 		vector<int> memo(32, 0);
         
         long temp = 1;
-		for (int index = 0; index < 32; index++, temp <<= 1)
+	for (int index = 0; index < 32; index++, temp <<= 1)
+	{
+		for (auto number : nums)
 		{
-			for (auto number : nums)
-			{
-				memo[index] += ((temp & number) >= 1);
-			}
-			memo[index] %= 3;
+			memo[index] += ((temp & number) >= 1);
 		}
+		memo[index] %= 3;
+	}
 
-		int res = 0;
-		for (int index = 31; index >= 0; index--)
-		{
-			res <<= 1;
-			res += memo[index];
-		}
-		return res;
+	int res = 0;
+	for (int index = 31; index >= 0; index--)
+	{
+		res <<= 1;
+		res += memo[index];
+	}
+	return res;
 	}
 };
